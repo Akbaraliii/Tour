0e72bda458c6fbd40644794e0beb937b7d191872
Akbaraliii <91048215+Akbaraliii@users.noreply.github.com>
2022-04-02 15:52:41 +0900

    first commit

---
+++ setup/src/App.js
@@ -0,0 +1,41 @@
+import React, { useState, useEffect } from 'react'
+import Loading from './Loading'
+import Tours from './Tours'
+
+const url = 'https://course-api.com/react-tours-project'
+function App() {
+  const [loading, setLoading] = useState(false)
+  const [tours, setTours] = useState([])
+
+  const fetchTours = async () => {
+    setLoading(true)
+
+    try {
+      const response = await fetch(url)
+      const tours = await response.json()
+      setLoading(false)
+      setTours(tours)
+    } catch (error) {
+      setLoading(false)
+      console.log(error)
+    }
+  }
+  useEffect(() => {
+    fetchTours()
+  }, [])
+
+  if (loading) {
+    return (
+      <main>
+        <Loading />
+      </main>
+    )
+  }
+  return (
+    <main>
+      <Tours />
+    </main>
+  )
+}
+
+export default App
