b5ad2fc9351048082da4afb78c1094cb6c7ce991
Akbaraliii <91048215+Akbaraliii@users.noreply.github.com>
2022-04-02 22:00:57 +0900

    Final one

--- setup/src/App.js
+++ setup/src/App.js
@@ -6,6 +6,10 @@ const url = 'https://course-api.com/react-tours-project'
 function App() {
   const [loading, setLoading] = useState(false)
   const [tours, setTours] = useState([])
+  const removeTour = (id) => {
+    const newTours = tours.filter((tour) => tour.id !== id)
+    setTours(newTours)
+  }
 
   const fetchTours = async () => {
     setLoading(true)
@@ -31,9 +35,21 @@ function App() {
       </main>
     )
   }
+  if (tours.length === 0) {
+    return (
+      <main>
+        <div className='title'>
+          <h2>no tours left</h2>
+          <button className='btn' onClick={fetchTours}>
+            Refresh
+          </button>
+        </div>
+      </main>
+    )
+  }
   return (
     <main>
-      <Tours tours={tours} />
+      <Tours tours={tours} removeTour={removeTour} />
     </main>
   )
 }
--- setup/src/Tour.js
+++ setup/src/Tour.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 
-const Tour = ({ id, image, info, price, name }) => {
+const Tour = ({ id, image, info, price, name, removeTour }) => {
   const [readMore, setReadMore] = useState(false)
   return (
     <article className='single-tour'>
@@ -8,7 +8,7 @@ const Tour = ({ id, image, info, price, name }) => {
       <footer>
         <div className='tour-info'>
           <h4>{name}</h4>
-          <h4 className='tour-price'>{price}</h4>
+          <h4 className='tour-price'>${price}</h4>
         </div>
         <p>
           {readMore ? info : `${info.substring(0, 200)}...`}
@@ -16,7 +16,9 @@ const Tour = ({ id, image, info, price, name }) => {
             {readMore ? 'show less' : 'read more'}
           </button>
         </p>
-        <button className='delete-btn'>not interested</button>
+        <button className='delete-btn' onClick={() => removeTour(id)}>
+          not interested
+        </button>
       </footer>
     </article>
   )
